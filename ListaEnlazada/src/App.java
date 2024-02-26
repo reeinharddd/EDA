ece91020c6c9ef937f55c8659d8ca4798d1a1ae1
Erik <beltraberik@gmail.com>
2024-02-26 10:24:42 -0800

    Trabajo

--- ListaEnlazada/src/App.java
+++ ListaEnlazada/src/App.java
@@ -5,11 +5,35 @@ public class App {
 
   public static void main(String[] args) throws Exception {
     Lista lista = new Lista();
+    Object dato, nuevoDato;
+    Scanner s = new Scanner(System.in);
     System.out.println("Insertar elementos al frente de la lista\n");
     lista.insertarAlFrente(1);
     lista.imprimir();
-    lista.insertarAlFrente(-1);
+    lista.insertarAlFinal(-1);
+    lista.imprimir();
     lista.insertarAlFrente(10);
-    lista.insertarAlFrente(-10);
+    lista.imprimir();
+    lista.insertarAlFinal(-10);
+    lista.imprimir();
+
+    // System.out.println("Valor a buscar: ");
+    // dato = s.nextInt();
+    // if (lista.buscarPorValor(dato) != null) {
+    //   System.out.println("El valor " + dato + " esta en la lista");
+    // } else {
+    //   System.out.println("El valor " + dato + " no esta en la lista");
+    // }
+
+    //Agregar con referencia
+    System.out.println("Insertar el nuevo dato\n");
+    nuevoDato = s.nextInt();
+    System.out.println("Insertar el dato de referencia\n");
+    dato = s.nextInt();
+
+    lista.insertarPorReferencia(nuevoDato, dato);
+    lista.imprimir();
+
+    s.close();
   }
 }
--- ListaEnlazada/src/ListaEnteros/Lista.java
+++ ListaEnlazada/src/ListaEnteros/Lista.java
@@ -15,6 +15,28 @@ public class Lista {
     primerNodo = ultimoNodo = null;
   }
 
+  public void insertarPorReferencia(Object dato, Object referencia) {
+    Nodo nuevo;
+    Nodo anterior = buscarPorValor(referencia);
+    if (anterior != null) {
+      nuevo = new Nodo(dato);
+      nuevo.siguienteNodo = anterior.siguienteNodo;
+      anterior.siguienteNodo = nuevo;
+    } else {
+      System.out.println("No se encontro el valor de referencia");
+    }
+  }
+
+  public Nodo buscarPorValor(Object destino) {
+    Nodo indice;
+    for (indice = primerNodo; indice != null; indice = indice.siguienteNodo) {
+      if (indice.dato.equals(destino)) {
+        return indice;
+      }
+    }
+    return null;
+  }
+
   public void insertarAlFrente(Object elemento) {
     if (estaVacia()) {
       primerNodo = ultimoNodo = new Nodo(elemento);
